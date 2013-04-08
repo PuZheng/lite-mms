7447c61e70574325252c60d44b428e71c27bd4a5
abc549825 <abc549825@163.com>
2013-04-08 09:32:42 +0800

    import cargo

--- lite_mms/constants/cargo.py
+++ lite_mms/constants/cargo.py
@@ -6,7 +6,7 @@ STATUS_CLOSED = 3
 STATUS_DISMISSED = 4
 
 ACT_LOAD = 1
-ACT_WEIGH = 2
+ACT_WEIGHT = 2
 ACT_CLOSE = 3
 ACT_OPEN = 4
 ACT_DISMISS = 5
@@ -15,7 +15,7 @@ ACT_DISMISS = 5
 def desc_action(action):
     if action == ACT_LOAD:
         return u"装货"
-    elif action == ACT_WEIGH:
+    elif action == ACT_WEIGHT:
         return u"称重"
     elif action == ACT_CLOSE:
         return u"关闭"

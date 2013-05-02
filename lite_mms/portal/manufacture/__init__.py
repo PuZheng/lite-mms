9cd04f8150e2f46cbda59995646d35d06121a0cd
abc549825 <abc549825@163.com>
2013-05-02 17:18:51 +0800

    patch

--- lite_mms/portal/manufacture/__init__.py
+++ lite_mms/portal/manufacture/__init__.py
@@ -1,15 +1,34 @@
-from flask import Blueprint, request
-from lite_mms.permissions.work_command import view_work_command
+# -*- coding: UTF-8 -*-
+from flask import Blueprint, redirect, url_for, json
 
 manufacture_page = Blueprint("manufacture", __name__, static_folder="static",
                              template_folder="templates")
 
-@manufacture_page.before_request
-def _():
-    pass
-    # TODO need fine-grined permissions here
-    #if not request.path.startswith("/manufacture/work-command/"):
-        #with view_work_command.require():
-            #pass
-
 from lite_mms.portal.manufacture import views, ajax
+
+from lite_mms.apis.manufacture import get_department_list
+
+from lite_mms.basemain import data_browser, nav_bar
+
+department_list = dict((d.id, [dict(id=p.id, text=p.name) for p in d.procedure_list]) for d in get_department_list())
+
+def _do_register(model_view):
+    extra_params = {
+        "list_view": {
+            "nav_bar": nav_bar,
+            "titlename": model_view.model_name + u"列表",
+        },
+        "form_view": {
+            "nav_bar": nav_bar,
+            "titlename": u"编辑" + model_view.model_name,
+            "department_list": department_list
+        }
+
+    }
+    data_browser.register_model_view(model_view, manufacture_page, extra_params)
+
+_do_register(views.work_command_view)
+
+@manufacture_page.route("/")
+def index():
+    return redirect(url_for("manufacture.work_command_list"))

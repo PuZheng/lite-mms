0b7cbc43845efecff0ec60e6f30aa798b8784746
xiechao06 <xiechao06@gmail.com>
2013-05-15 16:19:21 +0800

    0

--- lite_mms/test/at/test_cargo.py
+++ lite_mms/test/at/test_cargo.py
@@ -5,54 +5,64 @@ from lite_mms.database import db
 
 def test():
     from pyfeature import flask_sqlalchemy_setup
-    flask_sqlalchemy_setup(app, db, create_step_prefix=u"创建")
+    flask_sqlalchemy_setup(app, db, create_step_prefix=u"创建", 
+                           model_name_getter=lambda model: model.__modelname__, 
+                           attr_name_getter=lambda model, attr: model.__col_desc__.get(attr, attr),
+                           set_step_pattern=u'(\w+)([\.\w+]+)设置为(.+)',
+                           test_step_pattern=u'(\w+)([\.\w+]+)是(.+)')
 
     with Feature("卸货会话生成发货单", ['lite_mms.test.at.steps.cargo']):
         with Scenario(u"最简完整流程"):
-            when(u'收发员创建Plate(浙B 00001)')
-            and_(u'收发员创建UnloadSession， 毛重是10000公斤')
-            and_(u'装卸工进行卸货，该货物来自宁波机床厂，并且标记是最后一个任务')
-            and_(u'收发员称重8000公斤')
+            plate = when(u"创建车辆(浙B 00001)")
+            and_(u'车辆.车牌号设置为u"浙B 00002"', plate)
+            then(u'车辆.车牌号是u"浙B 00002"', plate)
+            then(u'车辆.车牌号是s', plate, s=u"浙B 00002")
+            #when(u'收发员创建Plate(浙B 00001)')
+            #and_(u'收发员创建UnloadSession， 毛重是10000公斤')
+            #and_(u'装卸工进行卸货，该货物来自宁波机床厂，并且标记是最后一个任务')
+            #and_(u'收发员称重8000公斤')
 
-            then(u"卸货任务的重量是2000公斤")
-            and_(u'卸货任务已经关闭')
+            #then(u"卸货任务的重量是2000公斤")
+            #and_(u'卸货任务已经关闭')
 
-            when(u'收发员生成收货单')
-            then(u'该收货单中包含一个项目，该项目的客户是宁波机床厂, 项目的重量是2000公斤')
+            #when(u'收发员生成收货单')
+            #then(u'该收货单中包含一个项目，该项目的客户是宁波机床厂, 项目的重量是2000公斤')
 
         with Scenario(u"包含多次卸货任务的卸货会话"):
-            when(u'收发员创建Plate(浙B 00001)')
-            and_(u'收发员创建UnloadSession， 毛重是10000公斤')
-            and_(u'装卸工进行卸货，该货物来自宁波机床厂')
-            then(u'装卸工此时不能进行卸货')
+            pass
+            #when(u'收发员创建Plate(浙B 00001)')
+            #and_(u'收发员创建UnloadSession， 毛重是10000公斤')
+            #and_(u'装卸工进行卸货，该货物来自宁波机床厂')
+            #then(u'装卸工此时不能进行卸货')
 
-            when(u'收发员称重8000公斤')
-            then(u"卸货任务的重量是2000公斤")
-            and_(u'卸货任务没有关闭')
+            #when(u'收发员称重8000公斤')
+            #then(u"卸货任务的重量是2000公斤")
+            #and_(u'卸货任务没有关闭')
 
-            when(u'装卸工进行卸货，该货物来自宁力紧固件, 并且标记是最后一个任务')
-            and_(u'收发员称重5000公斤')
-            then(u'收货任务的重量是3000公斤')
-            and_(u"卸货会话已经关闭")
+            #when(u'装卸工进行卸货，该货物来自宁力紧固件, 并且标记是最后一个任务')
+            #and_(u'收发员称重5000公斤')
+            #then(u'收货任务的重量是3000公斤')
+            #and_(u"卸货会话已经关闭")
 
-            when(u'收发员生成收货单')
-            then(u'该收货单中包含两个项目')
-            and_(u'第一个项目的客户是宁波机床厂, 项目的重量是2000公斤')
-            and_(u'第二个项目的客户是宁力紧固件, 项目的重量是3000公斤')
+            #when(u'收发员生成收货单')
+            #then(u'该收货单中包含两个项目')
+            #and_(u'第一个项目的客户是宁波机床厂, 项目的重量是2000公斤')
+            #and_(u'第二个项目的客户是宁力紧固件, 项目的重量是3000公斤')
 
         with Scenario(u'除非卸货会话关闭，否则卸货会话都可以修改'):
-            when(u'创建卸货会话, 其状态是待称重')
-            and_(u'创建卸货任务')
-            and_(u'修改卸货会话的车牌号为浙B 00002')
-            and_(u'修改卸货会话的毛重为10000公斤')
-            then(u'卸货会话的车牌号为浙B 00002') 
-            and_(u'卸货会话的重量为10000公斤')
-            and_(u'修改卸货任务的重量为2000公斤')
-            then(u'卸货任务的重量是2000公斤')
+            pass
+            #when(u'创建卸货会话, 其状态是待称重')
+            #and_(u'创建卸货任务')
+            #and_(u'修改卸货会话的车牌号为浙B 00002')
+            #and_(u'修改卸货会话的毛重为10000公斤')
+            #then(u'卸货会话的车牌号为浙B 00002') 
+            #and_(u'卸货会话的重量为10000公斤')
+            #and_(u'修改卸货任务的重量为2000公斤')
+            #then(u'卸货任务的重量是2000公斤')
 
-            when(u'关闭卸货会话')
-            then_(u'不能修改卸货会话')
-            and_(u'不能修改卸货任务')
+            #when(u'关闭卸货会话')
+            #then_(u'不能修改卸货会话')
+            #and_(u'不能修改卸货任务')
 
         with Scenario(u'收发员删除最后一个未称重的卸货任务'):
             pass
@@ -67,17 +77,18 @@ def test():
             pass
 
         with Scenario(u'若收货单过时，或者已经生成了订单，那么不能修改收货单'):
-            when(u'创建卸货会话')
-            and_(u'创建卸货任务，客户是宁波机床厂')
-            and_(u'生成收货单')
-            and_(u'创建卸货任务，客户是宁波机床厂')
+            pass
+            #when(u'创建卸货会话')
+            #and_(u'创建卸货任务，客户是宁波机床厂')
+            #and_(u'生成收货单')
+            #and_(u'创建卸货任务，客户是宁波机床厂')
 
-            then(u'收货单过时')
-            and_(u'不能修改收货单')
+            #then(u'收货单过时')
+            #and_(u'不能修改收货单')
             
-            when(u'重新生成收货单')
-            and_(u'生成订单')
-            then(u'不能修改收货单')
+            #when(u'重新生成收货单')
+            #and_(u'生成订单')
+            #then(u'不能修改收货单')
 
 
             

8d6c6187f6d7616d0edc2b23ec9a755b00f06862
www-data <www-data@localhost.(none)>
2013-05-16 20:25:09 +0800

    0

--- lite_mms/tools/deploy_web_hook.py
+++ lite_mms/tools/deploy_web_hook.py
@@ -15,10 +15,12 @@ OPTIONS
         the fabfile to execute
 """
 import sys
+import subprocess
+import json
 from getopt import getopt
 from flask import Flask, request
 
-opts, _ = getopt(sys.args[1:], "f:s:p:h")
+opts, _ = getopt(sys.argv[1:], "f:s:p:h")
 
 host = None
 port = None
@@ -29,6 +31,7 @@ for o, v in opts:
         port = int(v)
     elif o == "-h":
         print __doc__
+	sys.exit(-1)
     elif o == "-f":
         fabfile = v
     else:
@@ -47,7 +50,15 @@ app = Flask(__name__)
 @app.route("/deploy", methods=["POST"])
 def deploy():
     # we only deploy when push to origin/master
-    subprocess.call(["fab", "-f", fabfile, "deploy"])
+    import pudb; pudb.set_trace()
+    if json.loads(request.form['payload'])['ref'] == 'refs/heads/master':
+        subprocess.call(["fab", "-f", fabfile, "deploy"])
+    return "ok"
+
+@app.route("/make-test-data", methods=["POST"])
+def make_test_data():
+    # we only deploy when push to origin/master
+    subprocess.call(["fab", "-f", fabfile, "make_test_data"])
     return "ok"
 
 app.run(host=host, port=port)

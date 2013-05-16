7feec1293ee493ee248aaf2751f3263a829e7248
xiechao06 <xiechao06@gmail.com>
2013-05-16 20:59:47 +0800

    0

--- lite_mms/tools/deploy_web_hook.py
+++ lite_mms/tools/deploy_web_hook.py
@@ -20,39 +20,16 @@ import json
 from getopt import getopt
 from flask import Flask, request
 
-opts, _ = getopt(sys.argv[1:], "f:s:p:h")
-
-host = None
-port = None
-for o, v in opts:
-    if o == "-s":
-        host = v
-    elif o == "-p":
-        port = int(v)
-    elif o == "-h":
-        print __doc__
-	sys.exit(-1)
-    elif o == "-f":
-        fabfile = v
-    else:
-        print "unkown option: " + o
-        print __doc__
-        sys.exit(-1)
-
-try:
-    fabfile
-except NameError:
-    print __doc__
-    sys.exit(-1)
 
 app = Flask(__name__)
+
+fabfile = "fabfile.py"
 
 @app.route("/deploy", methods=["POST"])
 def deploy():
     # we only deploy when push to origin/master
-    import pudb; pudb.set_trace()
     if json.loads(request.form['payload'])['ref'] == 'refs/heads/master':
-        subprocess.call(["fab", "-f", fabfile, "deploy"])
+	subprocess.call(["fab", "-f", fabfile, "deploy"])
     return "ok"
 
 @app.route("/make-test-data", methods=["POST"])
@@ -61,4 +38,30 @@ def make_test_data():
     subprocess.call(["fab", "-f", fabfile, "make_test_data"])
     return "ok"
 
-app.run(host=host, port=port)
+#if __name__ == "__main__":
+#    opts, _ = getopt(sys.argv[1:], "f:s:p:h")
+#    
+#    host = None
+#    port = None
+#    for o, v in opts:
+#        if o == "-s":
+#            host = v
+#        elif o == "-p":
+#            port = int(v)
+#        elif o == "-h":
+#            print __doc__
+#    	sys.exit(-1)
+#        elif o == "-f":
+#            fabfile = v
+#        else:
+#            print "unkown option: " + o
+#            print __doc__
+#            sys.exit(-1)
+#    
+#    try:
+#        fabfile
+#    except NameError:
+#        print __doc__
+#        sys.exit(-1)
+#    app.run(host=host, port=port)
+

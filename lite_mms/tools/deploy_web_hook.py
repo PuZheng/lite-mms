319d08070e357ab7f95f15cae32eae0f8c7326f9
xiechao06 <xiechao06@gmail.com>
2013-05-17 10:58:25 +0800

    0

--- lite_mms/tools/deploy_web_hook.py
+++ lite_mms/tools/deploy_web_hook.py
@@ -27,8 +27,6 @@ fabfile = "fabfile.py"
 
 @app.route("/deploy", methods=["POST"])
 def deploy():
-    # we only deploy when push to origin/master
-    if json.loads(request.form['payload'])['ref'] == 'refs/heads/master':
 	subprocess.call(["fab", "-f", fabfile, "deploy"])
     return "done"
 
@@ -37,31 +35,4 @@ def make_test_data():
     # we only deploy when push to origin/master
     subprocess.call(["fab", "-f", fabfile, "make_test_data"])
     return "done"
-
-#if __name__ == "__main__":
-#    opts, _ = getopt(sys.argv[1:], "f:s:p:h")
-#    
-#    host = None
-#    port = None
-#    for o, v in opts:
-#        if o == "-s":
-#            host = v
-#        elif o == "-p":
-#            port = int(v)
-#        elif o == "-h":
-#            print __doc__
-#    	sys.exit(-1)
-#        elif o == "-f":
-#            fabfile = v
-#        else:
-#            print "unkown option: " + o
-#            print __doc__
-#            sys.exit(-1)
-#    
-#    try:
-#        fabfile
-#    except NameError:
-#        print __doc__
-#        sys.exit(-1)
-#    app.run(host=host, port=port)
 

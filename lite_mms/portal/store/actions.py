a31bada5796c10c0b134e90218e6e7b62a82e14b
abc549825 <abc549825@163.com>
2013-07-02 11:00:04 +0800

    fix #64

--- lite_mms/portal/store/actions.py
+++ lite_mms/portal/store/actions.py
@@ -1,9 +1,9 @@
 #-*- coding:utf-8 -*-
-from flask.ext.databrowser.action import BaseAction
+from flask.ext.databrowser.action import DirectAction
 from flask import redirect, url_for, request
 
 
-class PreviewPrintAction(BaseAction):
+class PreviewPrintAction(DirectAction):
     def op_upon_list(self, objs, model_view):
         if len(objs):
             return redirect(url_for("store_bill.store_bill_preview", id_=objs[0].id, url=request.url))

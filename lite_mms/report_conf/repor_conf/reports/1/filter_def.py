05eeb38ca1aef82d9d5b1610994e660194ae5668
xiechao06 <xiechao06@gmail.com>
2013-07-08 11:11:23 +0800

    0

--- lite_mms/report_conf/repor_conf/reports/1/filter_def.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-from datetime import datetime
-
-def get_filter(db, model_map):
-    today = datetime.today()
-    first_day_of_this_month = datetime(today.year, today.month, 1)
-    return WorkCommand.compeleted_time < first_day_of_this_month
-

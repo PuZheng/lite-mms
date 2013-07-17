ae950a9937bb100badfe0c9e9d296c5b5d2b9e80
xiechao06 <xiechao06@gmail.com>
2013-07-17 16:54:17 +0800

    add method passed_weight

--- lite_mms/apis/manufacture.py
+++ lite_mms/apis/manufacture.py
@@ -79,6 +79,10 @@ class WorkCommandWrapper(ModelWrapper):
     def to_delivery_weight(self):
         return sum(sum(sb.weight for sb in qir.store_bill_list if not sb.delivered) for qir in self.qir_list)
 
+    @property
+    def passed_weight(self):
+        return sum(qir.weight for qir in self.qir_list if qir.result==constants.quality_inspection.FINISHED)
+
     @classmethod
     def new_work_command(cls, sub_order_id, org_weight, org_cnt, procedure_id,
                          urgent, tech_req="", pic_path=""):

e49e39a893805458235472a1d7e8d5fbffe7185d
xiechao06 <xiechao06@gmail.com>
2013-09-05 16:35:38 +0800

    when db inexists, create it

--- lite_mms/database.py
+++ lite_mms/database.py
@@ -7,8 +7,15 @@ db = SQLAlchemy(app)
 from CodernityDB.database import Database
 
 codernity_db = Database(app.config['CODERNITY_DATABASE_PATH'])
-codernity_db.open()
-codernity_db.reindex()
+if codernity_db.exists():
+    codernity_db.open()
+    codernity_db.reindex()
+else:
+    codernity_db.create()
+    from lite_task_flow.indexes import add_index
+    add_index(codernity_db)
+    from lite_mms.indexes import TaskWithGroup
+    codernity_db.add_index(TaskWithGroup(codernity_db.path, TaskWithGroup.TAG))
 
 app.config["MONGODB_DB"] = "localhost"
 
